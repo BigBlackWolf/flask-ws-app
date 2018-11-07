0ad1d9c29b8b5da178c8e6ab9d6391646e74373e
Stepan Rasputny <stepan.rasputny@gmail.com>
2018-11-07 15:59:50 +0200

    Moved to postgres

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     CSRF_ENABLED = False
     SECRET_KEY = 'somesecretkey'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/messanger'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
